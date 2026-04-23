f2de75cd793f112b17d1ed74470f9b13efb8ffbd
Mohd farrakh nizam <113374040+farrakhnizam786@users.noreply.github.com>
2026-04-23 14:29:38 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
 // Create the connection pool
-const sql = neon(process.env.NEON_DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL);
 
 // Export for use in your application
 export default sql;
